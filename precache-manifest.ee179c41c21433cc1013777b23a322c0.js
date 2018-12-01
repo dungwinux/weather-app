ec1b6c52a264f6bde374893fc88dc4b5b386fa26
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-12-01 20:08:30 +0700

    Updates

---
+++ precache-manifest.ee179c41c21433cc1013777b23a322c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "d7a786bd0b4fe64a09ae",
+    "url": "/weather-app/static/js/main.d7a786bd.chunk.js"
+  },
+  {
+    "revision": "63bcfed1f46b6159d273",
+    "url": "/weather-app/static/js/1.63bcfed1.chunk.js"
+  },
+  {
+    "revision": "d7a786bd0b4fe64a09ae",
+    "url": "/weather-app/static/css/main.5bf7fafb.chunk.css"
+  },
+  {
+    "revision": "3f644a1bea916315e0c0e892dfaacb17",
+    "url": "/weather-app/index.html"
+  }
+];
