b313af056ed54ae248898601951f4a39903b1b1a
Nguyễn Tuấn Dũng <ntddebugger@gmail.com>
2018-12-01 20:18:34 +0700

    Fix code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./Weather";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root");
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.register();
